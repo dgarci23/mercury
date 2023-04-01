56679f16b4827df1462b713838fe7110a5e24388
David <dgarci23@nd.edu>
2023-03-31 23:01:22 -0400

    Post signup dynamodb integ

--- amplify/backend/function/mercurySignupLambda/src/index.js
+++ amplify/backend/function/mercurySignupLambda/src/index.js
@@ -6,5 +6,36 @@ exports.handler = (event, context, callback) => {
   console.log("LOG");
   console.log(event);
 
+  // DynamoDB configuration
+  AWS.config.update({ region: process.env.TABLE_REGION });
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const tableName = "users";
+
+  if (process.env.ENV && process.env.ENV !== "NONE") {
+    tableName = tableName + '-' + process.env.ENV;
+  }
+
+  // DynamoDB put parameters
+  const putItemParams = {
+    TableName: tableName,
+    Item: {
+      userId: event.userName,
+      name: event.request.userAttributes.name,
+      phone: event.request.userAttributes.phone_number,
+      email: event.request.userAttributes.email
+    }
+  }
+
+  // DynamoDB put request
+  dynamodb.put(putItemParams, (err, data) => {
+    if (err) {
+      console.log(`Unsuccessfull Put: ${err}`)
+      callback(null, event);
+    } else {
+      console.log("Successfull Put");
+      callback(null, event);
+    }
+  });
+
   callback(null, event);
 };

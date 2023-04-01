3ab41f6ebb06b2ff647a8d65c146bbc3b7ccf8de
David <dgarci23@nd.edu>
2023-04-01 00:04:29 -0400

    Fix React issue

--- src/App.js
+++ src/App.js
@@ -7,41 +7,59 @@ import Button from "@cloudscape-design/components/button";
 import { ColumnLayout } from "@cloudscape-design/components";
 import AddAdressModal from "./AddAdressModal";
 
+
+import {Authenticator} from "@aws-amplify/ui-react"
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 export default function App() {
   const [value, setValue] = useState("");
 
 
 
   return (
-    <SpaceBetween size="m">
-      <Container>
-        <ColumnLayout columns={2}>
-        <div>        
-          {/* put logo here */}
-          <Header variant="h1">Mercury</Header>
-        </div>
-        <div >
-          <Button style="profile-button" iconAlign="right" iconName="user-profile-active" variant="icon"/>
-        </div>
-        <div>
-          <Header variant="h5">the one stop shop for shipping adresses</Header>
-        </div>
-        </ColumnLayout>
+    <Authenticator>
+      {({signOut, user}) => {
+        return (
+          <div className="App">
+            <SpaceBetween size="m">
+              <Container>
+                <ColumnLayout columns={2}>
+                <div>        
+                  {/* put logo here */}
+                  <Header variant="h1">Mercury</Header>
+                </div>
+                <div >
+                  <Button style="profile-button" iconAlign="right" iconName="user-profile-active" variant="icon"/>
+                </div>
+                <div>
+                  <Header variant="h5">the one stop shop for shipping adresses</Header>
+                </div>
+                </ColumnLayout>
 
-      </Container>
+              </Container>
 
-      <Container>
-        <SpaceBetween size="s">
-          <span>Start editing to see some magic happen</span>
-          <Input
-            value={value}
-            onChange={(event) => setValue(event.detail.value)}
-          />
-          <Button variant="primary">Click me</Button>
-          <Button variant="primary">Button</Button>
-          <AddAdressModal></AddAdressModal>
-        </SpaceBetween>
-      </Container>
-    </SpaceBetween>
+              <Container>
+                <SpaceBetween size="s">
+                  <span>Start editing to see some magic happen</span>
+                  <Input
+                    value={value}
+                    onChange={(event) => setValue(event.detail.value)}
+                  />
+                  <Button variant="primary">Click me</Button>
+                  <Button variant="primary">Button</Button>
+                  <AddAdressModal></AddAdressModal>
+                </SpaceBetween>
+              </Container>
+            </SpaceBetween>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+          )
+      }}
+    </Authenticator>
+    
   );
 }

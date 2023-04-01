7d3f37d634a833bed672abadcdbaaa460f613129
Explosion 8or <ebartek@nd.edu>
2023-04-01 15:04:25 -0400

    working froms frontend. NEEDS API IMPLEMENTATION

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import ContentLayout from "@cloudscape-design/components/content-layout";
-import Container from "@cloudscape-design/components/container";
 import Header from "@cloudscape-design/components/header";
 import SpaceBetween from "@cloudscape-design/components/space-between";
-import Link from "@cloudscape-design/components/link";
 import Button from "@cloudscape-design/components/button";
-import Alert from "@cloudscape-design/components/alert";
-
+import MercuryLogo from "./resources/FullLogo_Transparent_NoBuffer.png"
 
 import {Authenticator} from "@aws-amplify/ui-react"
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify, Auth } from 'aws-amplify';
-import AddAdressModal from "./AddAdressModal";
+import AddAddressModal from "./Components/AddAddressModal";
 import awsconfig from './aws-exports';
+import './App.css';
+import CurrentAddressComponent from "./Components/CurrentAddressComponent";
 
 Amplify.configure(awsconfig);
 
@@ -20,38 +19,28 @@ export default function App() {
   const [value, setValue] = useState("");
 
 
-
   return (
+    
     <ContentLayout
       header={
         <SpaceBetween size="m">
           <Header
             variant="h1"
-            info={<Link>Info</Link>}
-            description="This is a generic description used in the header."
             actions={
-              <Button variant="primary">Button</Button>
+              <Button className="vertical-center" variant="primary" iconName="user-profile-active">Profile</Button>
             }
           >
-            Mercury
-          </Header>
+            <div className="Logo-Container">
+              <img  src={MercuryLogo} className="Logo-Image" alt="logo" />
+            </div>
 
+
+          </Header>
         </SpaceBetween>
       }
     >
-      <Container
-        header={
-          <Header
-            variant="h2"
-            description="Container description"
-          >
-            Container header
-          </Header>
-        }
-      >
-        Container content
-      </Container>
-      <AddAdressModal></AddAdressModal>
+      <CurrentAddressComponent></CurrentAddressComponent>
+
     </ContentLayout>
   );
 }

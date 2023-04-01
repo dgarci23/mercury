b9d494c110bea5b0c418350a8fb5525aeb56fe18
David Garcia Gonzalez <dgarci23@nd.edu>
2023-04-01 19:55:05 -0400

    Fix hierarchy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Button from "@cloudscape-design/components/button";
 import MercuryLogo from "./resources/FullLogo_Transparent_NoBuffer.png"
 import Box from "@cloudscape-design/components/box"
 
-import {Authenticator} from "@aws-amplify/ui-react"
+import { Authenticator, Grid } from "@aws-amplify/ui-react"
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify, Auth } from 'aws-amplify';
 import AddAddressModal from "./Components/AddAddressModal";
@@ -14,7 +14,7 @@ import ProfileModal from "./Components/ProfileModal";
 import CompanyModal from "./Components/CompanyModal";
 import awsconfig from './aws-exports';
 import './App.css';
-import CurrentAddressComponent from "./Components/CurrentAddressComponent";
+import Dashboard from "./Components/Dashboard";
 
 Amplify.configure(awsconfig);
 class App extends React.Component {
@@ -26,43 +26,43 @@ class App extends React.Component {
   render() {
     return (
       <Authenticator>
-        {({signOut, user}) => {
-  
+        {({ signOut, user }) => {
+
           //user = await Amplify.Auth.currentAuthenticatedUser();
           const token = user.signInUserSession.idToken.jwtToken;
           console.log(token);
-  
+
           return (
             <div className="App">
               <ContentLayout
-        header={
-          <SpaceBetween size="m">
-            <Header
-              variant="h1"
-              actions={
-                <Box padding="xxl">
-                  <SpaceBetween direction="horizontal" size="xs">
-                  <Button variant="primary" iconName="user-profile-active">Profile</Button>
-                  <Button onClick={signOut}>Sign out</Button>
-                </SpaceBetween>
-                </Box>
-              }
-            >
-              <div className="Logo-Container">
-                <img  src={MercuryLogo} className="Logo-Image" alt="logo" />
-              </div>
-  
-  
-            </Header>
-          </SpaceBetween>
-        }
-      >
-        <CurrentAddressComponent></CurrentAddressComponent>
-        <CompanyModal></CompanyModal>
-      </ContentLayout>
-              
+                header={
+                  <SpaceBetween size="m">
+                    <Header
+                      variant="h1"
+                      actions={
+                        <Box padding="xxl">
+                          <SpaceBetween direction="horizontal" size="xs">
+                            <Button variant="primary" iconName="user-profile-active">Profile</Button>
+                            <Button onClick={signOut}>Sign out</Button>
+                          </SpaceBetween>
+                        </Box>
+                      }
+                    >
+                      <div className="Logo-Container">
+                        <img src={MercuryLogo} className="Logo-Image" alt="logo" />
+                      </div>
+
+
+                    </Header>
+                  </SpaceBetween>
+                }
+              >
+                <Dashboard></Dashboard>
+
+              </ContentLayout>
+
             </div>
-            )
+          )
         }}
       </Authenticator>
     );

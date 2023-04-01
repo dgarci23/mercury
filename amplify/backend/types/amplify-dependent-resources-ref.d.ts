eb709461e653e6136f53e9db1dfbb85865100211
David <dgarci23@nd.edu>
2023-03-31 21:27:33 -0400

    Add REST API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mercuryApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "mercuryApiLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "dynamoMercury": {
       "Arn": "string",
